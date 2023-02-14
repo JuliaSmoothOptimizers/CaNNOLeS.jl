d1b395fe2f6f730757f0b21e070bc5fbf8757bf6
tmigot <tangi.migot@gmail.com>
2023-02-14 12:40:47 -0500

    test small residual from initial guess

--- test/runtests.jl
+++ test/runtests.jl
@@ -133,6 +133,21 @@ end
   solve!(solver, nls, stats, check_small_residual = true)
   @test stats.status_reliable && stats.status == :small_residual
   @test stats.objective_reliable && isapprox(stats.objective, 0, atol = 1e-6)
+
+  nls = ADNLSModel(
+    x -> [x[1] - 1],
+    [-1.2; 1.0],
+    1,
+    x -> [10 * (x[2] - x[1]^2)],
+    zeros(1),
+    zeros(1),
+    name = "HS6",
+  )
+  stats = GenericExecutionStats(nls)
+  solver = CaNNOLeSSolver(nls)
+  solve!(solver, nls, stats, x = [0.99999, 0.99999], check_small_residual = true)
+  @test stats.status_reliable && stats.status == :small_residual
+  @test stats.objective_reliable && isapprox(stats.objective, 0, atol = 1e-6)
 end
 
 @testset "Re-solve with a different problem" begin
