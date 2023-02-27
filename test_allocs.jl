81395628f58f457de9deb510ec5749d1ee29fadf
tmigot <tangi.migot@gmail.com>
2023-02-27 09:08:31 -0500

    up script

--- test_allocs.jl
+++ test_allocs.jl
@@ -1,46 +1,33 @@
 using Pkg
 Pkg.activate(".")
-using CaNNOLeS, ADNLPModels, NLPModels, SolverCore
-
-#=
-nls = ADNLSModel(
-  x -> [x[1] - 1; 10 * (x[2] - x[1]^2)],
-  [-1.2; 1.0],
-  2,
-  x -> [x[1] * x[2] - 1],
-  [0.0],
-  [0.0],
-)
-=#
+using CaNNOLeS, NLPModels, SolverCore
+Pkg.status()
 
 include("mgh01con.jl")
 nls = MGH01CON()
 
-stats = GenericExecutionStats(nls)
-solver = CaNNOLeSSolver(nls)
-stats = cannoles(nls)
-x0 = zeros(2)
-λ = nls.meta.y0
-@allocated solve!(solver, nls, stats) # , x = x0, λ = λ)
-a = @allocated solve!(solver, nls, stats) # , x = x0, λ = λ) # 6720
-@show a
+stats, solver = GenericExecutionStats(nls), CaNNOLeSSolver(nls)
 
-#=
-64 without set_solver_specific !! (Keyword !!)
+nls.meta.x0 .= zeros(2)
+@allocated solve!(solver, nls, stats) # , x = x0, λ = λ)
+@show @allocated solve!(solver, nls, stats) # , x = x0, λ = λ) # 0
+reset!(nls); solve!(solver, nls, stats, verbose = 1)
+
+nls.meta.x0 .= [-1.2; 1]
+@allocated solve!(solver, nls, stats)
+a = @allocated solve!(solver, nls, stats) # 0
+@show a
+reset!(nls); solve!(solver, nls, stats, verbose = 1)
+
+#= TODO:
+- Use updated SolverCore version (merge solver_specific PR + new release)
+- Fix the initial Lagrange multiplier (right now it is [] and then we eval it)
 =#
 
-x0 = nls.meta.x0
-@allocated solve!(solver, nls, stats) # , x = x0, λ = λ)
-a = @allocated solve!(solver, nls, stats) # , x = x0, λ = λ) # 45248
-@show a
-
+#=
 using Profile
 Profile.Allocs.clear()
-Profile.Allocs.@profile sample_rate = 1 solve!(solver, nls, stats, x = x0, λ = λ)
+Profile.Allocs.@profile sample_rate = 1 solve!(solver, nls, stats)
 using PProf
 PProf.Allocs.pprof(from_c = false)
-
-#=
-a = 3856
-a = 34896
 =#
